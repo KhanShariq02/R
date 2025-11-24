4195665d1d8332018d7ba89daa5a7d92699c03c0
KhanShariq02 <shariq20702@gmail.com>
2025-11-24 23:58:08 +0530

    New function

--- Rmain.R
+++ Rmain.R
@@ -1,2 +1,11 @@
-dna <- "AGCT"
-dna
+calculate_annual_growth <- function(year_1 , year_2 , pop_y1 ,pop_y2,city) {
+    annual_growth <- (((pop_y2 - pop_y1) / pop_y1) * 100) / (year_2 - year_1)
+
+    message <- paste("From" ,year_1,"to",year_2, "the population of",city,
+    "grew by approx",annual_growth,"% each year")
+
+    print(message)
+    return(annual_growth)
+}
+
+calculate_annual_growth(2018,2020,10000000,15500000,"India")
