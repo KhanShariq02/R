2bba5f08e26338851a77d8b9cb9e8137f9003536
KhanShariq02 <shariq20702@gmail.com>
2025-11-25 00:07:37 +0530

    population_function

--- Rmain.R
+++ Rmain.R
@@ -1,11 +1,11 @@
 calculate_annual_growth <- function(year_1 , year_2 , pop_y1 ,pop_y2,city) {
     annual_growth <- (((pop_y2 - pop_y1) / pop_y1) * 100) / (year_2 - year_1)
 
-    message <- paste("From" ,year_1,"to",year_2, "the population of",city,
+    information <- paste("From" ,year_1,"to",year_2, "the population of",city,
     "grew by approx",annual_growth,"% each year")
 
-    print(message)
+    print(information)
     return(annual_growth)
 }
 
-calculate_annual_growth(2018,2020,10000000,15500000,"India")
+calculate_annual_growth(2018,2020,10000000,15500000,"Brazil")
