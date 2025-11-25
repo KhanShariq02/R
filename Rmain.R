42101cb84709ebb3d548315b4d3763c57149da1c
KhanShariq02 <shariq20702@gmail.com>
2025-11-25 23:36:31 +0530

    R_practice

--- Rmain.R
+++ Rmain.R
@@ -9,3 +9,24 @@ calculate_annual_growth <- function(year_1 , year_2 , pop_y1 ,pop_y2,city) {
 }
 
 calculate_annual_growth(2018,2020,10000000,15500000,"Brazil")
+
+#Reading and writing CSV files in R
+df <- data.frame(
+    sample_id = 1:5,
+    group = c("control","treatment","control","treatment","control"),
+    expr1 = c(10.5,12.3,9.8,11.7,10.1)
+)
+df
+
+write.csv(df , "my_data.csv", row.names = FALSE)
+
+new_df <- read.csv("my_data.csv")
+new_df
+str(new_df)
+summary(new_df)
+getwd()
+list.files()
+
+new_df
+new_df2 <- read.csv("my_data.csv" , stringsAsFactors = TRUE)
+new_df2
